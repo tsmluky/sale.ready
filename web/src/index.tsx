f54b3c4361e8d1632011d2d2e2c1525e50f4d240
tsmluky <tsmluky@gmail.com>
2026-01-02 22:45:21 -0300

    whitescreenfix

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Force Unregister Service Worker to fix Caching Issues (Blank Screen on F5)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (let registration of registrations) {
+      console.log('Unregistering SW:', registration);
+      registration.unregister();
+    }
+  });
+}
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
